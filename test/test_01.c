a15115ae4b60022e6dac6af54853720ed678bc3e
popov2 <andrey.o.popov@hotmail.com>
2021-10-26 08:45:00 +0300

    change test_01.c

--- test/test_01.c
+++ test/test_01.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../src/oilist.h"
+
 int main( int argc, char** argv )
 {
+    TList* pNewList = OiList_Create();
 
 
+    OiList_Destroy( pNewList, NULL );
 
     return 0;
 }
